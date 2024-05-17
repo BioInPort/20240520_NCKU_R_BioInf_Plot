2e88f2e965d8f70264b7ac103537113f7a3507fe
CharleneMSI <q2330416@gmail.com>
2024-05-17 18:37:34 -0500

    Presetting, Load packages

--- Scatterplots.R
+++ Scatterplots.R
@@ -1,15 +1,25 @@
 ## Ref: https://ggplot2.tidyverse.org/reference/geom_point.html
 
+#### Presetting ####
+rm(list = ls()) # Clean variable ##* Comment out if Run All
+memory.limit(300000)
 
 
+#### Load packages ####
+if(!require("ggplot2")) install.packages("ggplot2"); library(ggplot2)
 
 
+#### Load Data ####
 data <- data.frame(
   x = rnorm(50),
   y = rnorm(50)
 )
 
+#### Visualization ####
 p <- ggplot(data, aes(x=x, y=y)) +
   geom_point() +
   stat_summary(fun.data=mean_sdl, fun.args = list(mult=1), geom="errorbar", color="red")
 print(p)
+
+
+
