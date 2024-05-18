a6acbef1b4eeabca6af95406c597032e7cd7919a
CharleneMSI <q2330416@gmail.com>
2024-05-17 19:30:24 -0500

    Checkpoint

--- Scatterplots.R
+++ Scatterplots.R
@@ -10,13 +10,12 @@ if(!require("ggplot2")) install.packages("ggplot2"); library(ggplot2)
 
 
 #### Load Data ####
-data <- data.frame(
-  x = rnorm(50),
-  y = rnorm(50)
-)
+# data_csv <- read.csv("D:/Dropbox/##_GitHub/##_BioInPort/20240520_NCKU_R_BioInf_Plot/Demo_20240517/20240517191837_data.csv")
+# data_tsv <- read.table("D:/Dropbox/##_GitHub/##_BioInPort/20240520_NCKU_R_BioInf_Plot/Demo_20240517/20240517191837_data.tsv", sep = "\t", header = TRUE)
+data <- read.csv("D:/Dropbox/##_GitHub/##_BioInPort/20240520_NCKU_R_BioInf_Plot/Demo_20240517/20240517191837_data.csv")
 
 #### Visualization ####
-p <- ggplot(data, aes(x=x, y=y)) +
+p <- ggplot(data, aes(x=GeneA, y=GeneB)) +
   geom_point() +
   stat_summary(fun.data=mean_sdl, fun.args = list(mult=1), geom="errorbar", color="red")
 print(p)
@@ -26,6 +25,7 @@ print(p)
 Name_time_wo_micro <- substr(gsub("[- :]", "", as.character(Sys.time())), 1, 14)
 output_dir <- paste0(getwd(), "/Export_", substr(Name_time_wo_micro, 1, 8))
 if (!dir.exists(output_dir)) {dir.create(output_dir)}
+
 
 ## Export PDF
 pdf(file = paste0(output_dir,"/",Name_time_wo_micro,"_Scatterplots.pdf"), width = 7,  height = 7)
