cafe5f82d7fc3912f4bc675600e6605c244ff2c5
CharleneMSI <q2330416@gmail.com>
2024-05-18 03:34:57 -0500

    Add Heatmap

--- Demo_Dataset_Simulation.R
+++ Demo_Dataset_Simulation.R
@@ -8,9 +8,11 @@ rm(list = ls()) # Clean variable ##* Comment out if Run All
 #### Load packages ####
 if(!require("tidyverse")) install.packages("tidyverse"); library(tidyverse)
 
-
+####*************************************************************************####
+## Scatterplots
 #### Create data ####
 ## Basic scatterplots
+set.seed(123)
 data <- data.frame(
   x = rnorm(50),
   y = rnorm(50)
@@ -29,6 +31,35 @@ if (!dir.exists(output_dir)) {dir.create(output_dir)}
 write.csv(data, file = paste0(output_dir, "/", Name_time_wo_micro, "_data.csv"), row.names = FALSE)
 
 ## TSV
+write.table(data, file = paste0(output_dir, "/", Name_time_wo_micro, "_data.tsv"), sep = "\t", row.names = FALSE)
 
+
+####*************************************************************************####
+## Heatmap
+#### Create data ####
+## Basic Heatmap
+set.seed(123)
+mat <- matrix(rnorm(200), 20, 10)
+rownames(mat) <- paste("Gene", 1:20, sep="")
+colnames(mat) <- paste("Sample", 1:10, sep="")
+
+# Create annotation data for rows
+annotation_row <- data.frame(Class = sample(letters[1:3], 10, replace = TRUE))
+rownames(annotation_row) <- colnames(mat)
+
+
+
+#### Export ####
+## Set Expot ##
+Name_time_wo_micro <- substr(gsub("[- :]", "", as.character(Sys.time())), 1, 14)
+output_dir <- paste0(getwd(), "/Demo_", substr(Name_time_wo_micro, 1, 8))
+if (!dir.exists(output_dir)) {dir.create(output_dir)}
+
+
+## CSV
+write.csv(mat, file = paste0(output_dir, "/", Name_time_wo_micro, "_data_Heatmap.csv"), row.names = TRUE)
+
+## TSV
+write.table(data.frame(SamppleID= row.names(annotation_row) ,annotation_row), file = paste0(output_dir, "/", Name_time_wo_micro, "_data_Heatmap_Annot.tsv"), sep = "\t", row.names = FALSE)
 
 
